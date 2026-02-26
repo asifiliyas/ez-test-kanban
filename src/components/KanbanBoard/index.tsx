90a4a704af41abb2cd057932bc0c2a979ebe9ea4
Asif Iliyas <asif@opinium.ai>
2026-02-27 02:53:13 +0530

    Enhance touch support for drag & drop on mobile

--- src/components/KanbanBoard/index.tsx
+++ src/components/KanbanBoard/index.tsx
@@ -6,6 +6,7 @@ import {
     closestCorners,
     KeyboardSensor,
     PointerSensor,
+    TouchSensor,
     useSensor,
     useSensors,
     DragStartEvent,
@@ -37,6 +38,12 @@ export const KanbanBoard: React.FC = () => {
         useSensor(PointerSensor, {
             activationConstraint: {
                 distance: 8,
+            }
+        }),
+        useSensor(TouchSensor, {
+            activationConstraint: {
+                delay: 250,
+                tolerance: 5,
             }
         }),
         useSensor(KeyboardSensor, {
